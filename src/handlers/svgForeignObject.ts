3c141d0e78d9f4fe9290a8183f3f7042256dda03
p2r3 <p2r3@p2r3.com>
2026-01-23 02:17:28 +0200

    estimate document size when converting HTML to SVG

--- src/handlers/svgForeignObject.ts
+++ src/handlers/svgForeignObject.ts
@@ -31,13 +31,55 @@ class svgForeignObjectHandler implements FormatHandler {
     this.ready = true;
   }
 
-  static htmlToXML (html: string) {
+  static async normalizeHTML (html: string) {
+    // To get the size of the input document, we need the
+    // browser to actually render it.
+    // Create a hidden "dummy" element on the DOM.
     const dummy = document.createElement("div");
-    dummy.innerHTML = html;
+    dummy.style.all = "initial";
+    dummy.style.visibility = "hidden";
+    dummy.style.position = "fixed";
+    document.body.appendChild(dummy);
+
+    // Add a DOM shadow to the dummy to "sterilize" it.
+    // We also clear all styles within the shadow to be doubly sure.
+    const shadow = dummy.attachShadow({ mode: "closed" });
+    const style = document.createElement("style");
+    style.textContent = "*{all:initial;box-sizing:border-box;}";
+    shadow.appendChild(style);
+
+    // Create a div within the shadow DOM to act as
+    // a container for our HTML payload.
+    const container = document.createElement("div");
+    container.innerHTML = html;
+    shadow.appendChild(container);
+
+    // Wait for all images to finish loading. This is required for layout
+    // changes, not because we actually care about the image contents.
+    const images = container.querySelectorAll("img");
+    const promises = Array.from(images).map(image => new Promise(resolve => {
+      image.addEventListener("load", resolve);
+      image.addEventListener("error", resolve);
+    }));
+    await Promise.all(promises);
+
+    // Make sure the browser has had time to render.
+    // This is probably redundant due to the async calls above.
+    await new Promise(resolve => {
+      requestAnimationFrame(() => {
+        requestAnimationFrame(resolve);
+      });
+    });
+
+    // Finally, get the bounding box of the input and serialize it to XML.
+    const bbox = container.getBoundingClientRect();
     const serializer = new XMLSerializer();
-    const xml = serializer.serializeToString(dummy);
+    const xml = serializer.serializeToString(container);
+
+    container.remove();
     dummy.remove();
-    return xml;
+
+    return { xml, bbox };
   }
 
   async doConvert (
@@ -57,10 +99,11 @@ class svgForeignObjectHandler implements FormatHandler {
     for (const inputFile of inputFiles) {
       const { name, bytes } = inputFile;
       const html = decoder.decode(bytes);
+      const { xml, bbox } = await svgForeignObjectHandler.normalizeHTML(html);
       const svg = (
-`<svg width="1920" height="1080" xmlns="http://www.w3.org/2000/svg">
-<foreignObject x="0" y="0" width="1920" height="1080">
-${svgForeignObjectHandler.htmlToXML(html)}
+`<svg width="${bbox.width}" height="${bbox.height * 1.5}" xmlns="http://www.w3.org/2000/svg">
+<foreignObject x="0" y="0" width="${bbox.width}" height="${bbox.height * 1.5}">
+${xml}
 </foreignObject>
 </svg>`);
       const outputBytes = encoder.encode(svg);
